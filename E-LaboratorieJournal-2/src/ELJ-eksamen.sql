81e67cdbf1eaf7c9e6fd6068fabf7e0e0867341a
Bruger <Bruger@10.153.238.17>
2019-05-09 16:01:52 +0200

    SQL script opdateret. Alle tabeller oprettet og fungerende

--- E-LaboratorieJournal-2/src/ELJ-eksamen.sql
+++ E-LaboratorieJournal-2/src/ELJ-eksamen.sql
@@ -7,173 +7,168 @@ go
 
 use ELJ;
 
-create table student(
-studentID int identity NOT NULL,
-studentName nvarchar NOT NULL,
-classID int NOT NULL,
-
-primary key(studentID)
-foreign key(classID) REFERENCES class(classID)
-)
-
 create table class(
 classID int identity NOT NULL,
-className nvarchar NOT NULL,
+className nvarchar(max) NOT NULL,
 
 primary key(classID)
+)
+
+create table projectInfo(
+projectID int identity NOT NULL,
+dateCreated int NOT NULL,
+themeName nvarchar(max) NOT NULL,
+analyzeTitle nvarchar(max) NOT NULL,
+comment nvarchar(max) NOT NULL,
+
+primary key(projectID)
+)
+
+------------------- Above isn't dependent of anything -----------------
+
+create table student(
+studentID int identity NOT NULL,
+studentName nvarchar(max)(max) NOT NULL,
+classID int NOT NULL,
+
+primary key(studentID),
+
+foreign key (classID) REFERENCES class(classID)
+)
+
+create table picture(
+pictureID int identity NOT NULL,
+picture image NOT NULL,
+
+primary key(pictureID)
+)
+
+create table journal(
+journalID int identity NOT NULL,
+studentID int NOT NULL,
+projectID int NOT NULL,
+pictureID int NOT NULL,
+formID int NOT NULL,
+coworker nvarchar(max) NOT NULL,
+traceability nvarchar(max) NOT NULL,
+results nvarchar(max) NOT NULL,
+calculations nvarchar(max) NOT NULL,
+calcImage image NOT NULL,
+
+primary key(journalID),
+
+foreign key(studentID) REFERENCES student(studentID),
+foreign key(projectID) REFERENCES projectInfo(projectID),
+foreign key(pictureID) REFERENCES picture(pictureID)
+)
+
+create table form(
+formID int identity NOT NULL,
+studentID int NOT NULL,
+projectID int NOT NULL,
+formTypeID int NOT NULL,
+reagentName nvarchar(max) NOT NULL,
+
+primary key(formID),
+
+foreign key(studentID) REFERENCES student(studentID),
+foreign key(projectID) REFERENCES projectInfo(projectID),
+)
+
+create table substanceInfo(
+substanceID int identity NOT NULL,
+batchNo int NOT NULL,
+lotNo nvarchar(max) NOT NULL,
+supplier nvarchar(max) NOT NULL,
+
+primary key(substanceID)
+)
+
+create table preparation(
+preparationID int identity NOT NULL,
+substanceID int NOT NULL,
+formID int NOT NULL,
+chemical nvarchar(max) NOT NULL,
+casNo nvarchar(max) NOT NULL,
+productNo nvarchar(max) NOT NULL,
+weighed nvarchar(max) NOT NULL,
+measured nvarchar(max) NOT NULL,
+scaleNo nvarchar(max) NOT NULL,
+pipetteNo nvarchar(max) NOT NULL,
+endConcentration bit NOT NULL,
+
+primary key(preparationID),
+
+foreign key (formID) REFERENCES form(formID),
+foreign key (substanceID) REFERENCES substanceInfo(substanceID)
+)
+
+create table form1(
+formTypeID int identity NOT NULL,
+formID int NOT NULL,
+substanceID int NOT NULL,
+volume nvarchar(max) NOT NULL,
+accConcentration nvarchar(max) NOT NULL,
+lifeTimeF nvarchar(max) NOT NULL,
+storage nvarchar(max) NOT NULL,
+
+primary key(formTypeID),
+
+foreign key(formID) REFERENCES form(formID),
+foreign key(substanceID) REFERENCES substanceInfo(substanceID)
+)
+
+create table form2(
+formTypeID int identity NOT NULL,
+formID int NOT NULL,
+preparationID int NOT NULL,
+pHSetting nvarchar(max) NOT NULL,
+liquid nvarchar(max) NOT NULL,
+treatment nvarchar(max) NOT NULL,
+tag nvarchar(max) NOT NULL,
+lifeTimeF nvarchar(max) NOT NULL,
+storage nvarchar(max) NOT NULL,
+
+primary key(formID),
+
+foreign key(formID) REFERENCES form(formID)
+)
+
+create table form3(
+formTypeID int identity NOT NULL,
+formID int NOT NULL,
+preparationID int NOT NULL,
+pHSubstrate nvarchar(max) NOT NULL,
+pHSterialized nvarchar(max) NOT NULL,
+liquid nvarchar(max) NOT NULL,
+sterializeTime nvarchar(max) NOT NULL,
+sterializeC nvarchar(max) NOT NULL,
+addAftSterialize nvarchar(max) NOT NULL,
+pHAftSterialize nvarchar(max) NOT NULL,
+sterile bit NOT NULL,
+posControl nvarchar(max) NOT NULL,
+negControl nvarchar(max) NOT NULL,
+
+primary key(formID),
+
+foreign key(formID) REFERENCES form(formID)
+)
+
+-------------------- Relations -----------------
+
+create table journalForm(
+journalID int NOT NULL,
+formID int NOT NULL,
+
+primary key (journalID, formID),
 )
 
 create table studentForm(
 studentID int NOT NULL,
 formID int NOT NULL,
 
-primary key(studentID, formID)
-
-foreign key(studentID) REFERENCES student(studentID)
-foreign key(formID) REFERENCES form(formID)
+primary key(studentID, formID),
 )
 
------------------------------------------------
-
-create table journal(
-journalID int identity NOT NULL,
-studentID int NOT NULL,
-projectID int NOT NULL,
-formID int NOT NULL,
-coworker nvarchar NOT NULL,
-traceability nvarchar NOT NULL,
-results nvarchar NOT NULL,
-resultImage image NOT NULL,
-calculations nvarchar NOT NULL,
-calcImage image NOT NULL,
-
-primary key(journalID)
-
-
-foreign key(studentID) REFERENCES student(studentID)
-foreign key(projectID) REFERENCES projectInfo(projectID)
-foreign key(formID) REFERENCES picture(formID)
-)
-
-create table picture(
-pictureID int identity NOT NULL,
-journalID int NOT NULL,
-picture image NOT NULL,
-
-primary key(pictureID)
-
-foreign key(journalID) REFERENCES journal(journalID)
-)
-
------------------------------------------------
-
-create table journalForm(
-journalID int NOT NULL,
-formID int NOT NULL,
-
-primary key (journalID, formID)
-
-foreign key(journalID) REFERENCES journal(journalID)
-foreign key(formID) REFERENCES form(formID)
-)
-
-create table projectInfo(
-projectID int identity NOT NULL,
-dateCreated int NOT NULL,
-themeName nvarchar NOT NULL,
-analyzeTitle nvarchar NOT NULL,
-comment nvarchar NOT NULL,
-
-primary key(projectID)
-)
-
-----------------------------------------------
-
-create table form(
-formID int identity NOT NULL,
-studentID int NOT NULL,
-projectID int NOT NULL,
-journalID int NOT NULL,
-reagentName nvarchar NOT NULL,
-formTypeID int NOT NULL,
-
-primary key(formID)
-
-foreign key(studentID) REFERENCES student(studentID)
-foreign key(projectID) REFERENCES projectInfo(projectID)
-foreign key(journalID) REFERENCES journal(journalID)
-foreign key(formTypeID) REFERENCES form1(formTypeID)
-foreign key(formTypeID) REFERENCES form2(formTypeID)
-foreign key(formTypeID) REFERENCES form3(formTypeID)
-)
-
-create table form1(
-formTypeID int identity NOT NULL,
-volume nvarchar NOT NULL,
-accConcentration nvarchar NOT NULL,
-lifeTimeF nvarchar NOT NULL,
-storage nvarchar NOT NULL,
-
-primary key(formTypeID)
-)
-
-create table form2(
-formTypeID int identity NOT NULL,
-preperationID int NOT NULL,
-pHSetting nvarchar NOT NULL,
-liquid nvarchar NOT NULL,
-treatment nvarchar NOT NULL,
-tag nvarchar NOT NULL,
-lifeTimeF nvarchar NOT NULL,
-storage nvarchar NOT NULL,
-
-primary key(formTypeID)
-
-foreign key(preparationID) REFERENCES preparation(preparationID)
-)
-
-create table form3(
-formTypeID int identity NOT NULL,
-preperationID int NOT NULL,
-pHSubstrate nvarchar NOT NULL,
-pHSterialized nvarchar NOT NULL,
-liquid nvarchar NOT NULL,
-sterializeTime nvarchar NOT NULL,
-sterializeC nvarchar NOT NULL,
-addAftSterialize nvarchar NOT NULL,
-pHAftSterialize nvarchar NOT NULL,
-sterile bit NOT NULL,
-posControl nvarchar NOT NULL,
-negControl nvarchar NOT NULL,
-
-primary key(formTypeID)
-
-foreign key(preparationID) REFERENCES preparation(preparationID)
-)
-
-create table preparation(
-preparationID int identity NOT NULL,
-formID int NOT NULL,
-chemical nvarchar NOT NULL,
-casNo nvarchar NOT NULL,
-productNo nvarchar NOT NULL,
-weighed nvarchar NOT NULL,
-measured nvarchar NOT NULL,
-scaleNo nvarchar NOT NULL,
-pipetteNo nvarchar NOT NULL,
-endConcentration bit NOT NULL,
-
-primary key(preparationID)
-
-foreign key(formID) REFERENCES form2(formID)
-foreign key(formID) REFERENCES form3(formID)
-)
-
-create table substanceInfo(
-substanceID int identity NOT NULL,
-batchNo int NOT NULL,
-lotNo nvarchar NOT NULL,
-supplier nvarchar NOT NULL,
-
-primary key(substanceID)
-)
+INSERT INTO class VALUES ('DMU');
+INSERT INTO student VALUES ('akasdl', 1)
