e2a46fea3d036cdc164cb759ff0c469a4bfda632
Bruger <Bruger@10.153.238.31>
2019-05-13 14:34:24 +0200

    Ny Database SQL script

--- E-LaboratorieJournal-2/src/ELJ-DB.sql
+++ E-LaboratorieJournal-2/src/ELJ-DB.sql
@@ -1,11 +1,12 @@
 use master;
-if db_id('ELJ') IS NOT NULL
-drop database ELJ;
+if db_id('ELJ-DB') IS NOT NULL
+drop database [ELJ-DB];
 
-create database ELJ;
+create database [ELJ-DB];
 go
 
-use ELJ;
+use [ELJ-DB];
+
 
 create table class(
 classID int identity NOT NULL,
@@ -13,6 +14,7 @@ className nvarchar(max) NOT NULL,
 
 primary key(classID)
 )
+
 
 create table projectInfo(
 projectID int identity NOT NULL,
@@ -24,7 +26,9 @@ comment nvarchar(max) NOT NULL,
 primary key(projectID)
 )
 
+
 ------------------- Above isn't dependent of anything -----------------
+
 
 create table student(
 studentID int identity NOT NULL,
@@ -36,18 +40,14 @@ primary key(studentID),
 foreign key (classID) REFERENCES class(classID)
 )
 
-create table picture(
-pictureID int identity NOT NULL,
-picture image NOT NULL,
 
-primary key(pictureID)
-)
+------------------- Below is right side of Data Diagram -----------------
+
 
 create table journal(
 journalID int identity NOT NULL,
 studentID int NOT NULL,
 projectID int NOT NULL,
-pictureID int NOT NULL,
 formID int NOT NULL,
 coworker nvarchar(max) NOT NULL,
 traceability nvarchar(max) NOT NULL,
@@ -58,9 +58,23 @@ calcImage image NOT NULL,
 primary key(journalID),
 
 foreign key(studentID) REFERENCES student(studentID),
-foreign key(projectID) REFERENCES projectInfo(projectID),
-foreign key(pictureID) REFERENCES picture(pictureID)
+foreign key(projectID) REFERENCES projectInfo(projectID)
 )
+
+
+create table picture(
+pictureID int identity NOT NULL,
+journalID int NOT NULL,
+picture image NOT NULL,
+
+primary key(pictureID),
+
+foreign key(journalID) REFERENCES journal(journalID)
+)
+
+
+------------------- Below is left side of Data Diagram -----------------
+
 
 create table form(
 formID int identity NOT NULL,
@@ -68,6 +82,9 @@ studentID int NOT NULL,
 projectID int NOT NULL,
 formTypeID int NOT NULL,
 reagentName nvarchar(max) NOT NULL,
+batchNo int NOT NULL,
+lotNo nvarchar(max) NOT NULL,
+supplier nvarchar(max) NOT NULL,
 
 primary key(formID),
 
@@ -75,18 +92,9 @@ foreign key(studentID) REFERENCES student(studentID),
 foreign key(projectID) REFERENCES projectInfo(projectID),
 )
 
-create table substanceInfo(
-substanceID int identity NOT NULL,
-batchNo int NOT NULL,
-lotNo nvarchar(max) NOT NULL,
-supplier nvarchar(max) NOT NULL,
-
-primary key(substanceID)
-)
 
 create table preparation(
 preparationID int identity NOT NULL,
-substanceID int NOT NULL,
 formID int NOT NULL,
 chemical nvarchar(max) NOT NULL,
 casNo nvarchar(max) NOT NULL,
@@ -96,17 +104,17 @@ measured nvarchar(max) NOT NULL,
 scaleNo nvarchar(max) NOT NULL,
 pipetteNo nvarchar(max) NOT NULL,
 endConcentration bit NOT NULL,
+liquidAd nvarchar(max) NOT NULL,
 
 primary key(preparationID),
 
 foreign key (formID) REFERENCES form(formID),
-foreign key (substanceID) REFERENCES substanceInfo(substanceID)
 )
+
 
 create table form1(
 formTypeID int identity NOT NULL,
 formID int NOT NULL,
-substanceID int NOT NULL,
 volume nvarchar(max) NOT NULL,
 accConcentration nvarchar(max) NOT NULL,
 lifeTimeF nvarchar(max) NOT NULL,
@@ -114,16 +122,15 @@ storage nvarchar(max) NOT NULL,
 
 primary key(formTypeID),
 
-foreign key(formID) REFERENCES form(formID),
-foreign key(substanceID) REFERENCES substanceInfo(substanceID)
+foreign key(formID) REFERENCES form(formID)
 )
+
 
 create table form2(
 formTypeID int identity NOT NULL,
 formID int NOT NULL,
 preparationID int NOT NULL,
 pHSetting nvarchar(max) NOT NULL,
-liquid nvarchar(max) NOT NULL,
 treatment nvarchar(max) NOT NULL,
 tag nvarchar(max) NOT NULL,
 lifeTimeF nvarchar(max) NOT NULL,
@@ -134,13 +141,13 @@ primary key(formID),
 foreign key(formID) REFERENCES form(formID)
 )
 
+
 create table form3(
 formTypeID int identity NOT NULL,
 formID int NOT NULL,
 preparationID int NOT NULL,
 pHSubstrate nvarchar(max) NOT NULL,
 pHSterialized nvarchar(max) NOT NULL,
-liquid nvarchar(max) NOT NULL,
 sterializeTime nvarchar(max) NOT NULL,
 sterializeC nvarchar(max) NOT NULL,
 addAftSterialize nvarchar(max) NOT NULL,
@@ -154,7 +161,9 @@ primary key(formID),
 foreign key(formID) REFERENCES form(formID)
 )
 
--------------------- Relations -----------------
+
+-------------------- Below is relationtables -----------------
+
 
 create table journalForm(
 journalID int NOT NULL,
@@ -163,6 +172,7 @@ formID int NOT NULL,
 primary key (journalID, formID),
 )
 
+
 create table studentForm(
 studentID int NOT NULL,
 formID int NOT NULL,
@@ -170,5 +180,9 @@ formID int NOT NULL,
 primary key(studentID, formID),
 )
 
-INSERT INTO class VALUES ('DMU');
-INSERT INTO student VALUES ('frank', 1)
+
+--Test af indsætning
+
+
+--INSERT INTO class VALUES ('DMU');
+--INSERT INTO student VALUES ('akasdl', 1)
